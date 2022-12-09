a5a7ae713c3ff3236b6584e41904d125df830a0d
joanne muthoni <joanne.mumbi@student.moringaschool.com>
2022-12-09 20:15:53 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
 /* Your Code Here */
+function  createEmployeeRecord(row){
+    return {
+        firstName: row[0],
+        familyName: row[1],
+        title: row[2],
+        payPerHour: row[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+function createEmployeeRecords (array){
+    let twoRows = array.map((record)=>{
+        return createEmployeeRecord(record)
+    })
+    return twoRows
+    }
+   
+ function createTimeInEvent(employee,dateStamp ){
+    let hour = parseInt(dateStamp.slice(-4))
+    let date = dateStamp.slice(0, 10)
+    let type= "TimeIn"
+    employee.timeInEvents.push({
+        date:date,
+        type:type,
+        hour:hour
+    })
+    //YYYY-MM-DD HHMM
+   return employee  
+ }
+ 
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
